2ac04c1594f7788c8b591664759081c2d541a0eb
EdwardJones002 <160727416+EdwardJones002@users.noreply.github.com>
2024-12-28 12:46:44 +0000

    Cert Schema Added
    Certification Insert Scheme has been added, 104 certifications instantly present in the DB

--- src/main/java/com/user/certifications/config/DataInitializer.java
+++ src/main/java/com/user/certifications/config/DataInitializer.java
@@ -1,11 +1,23 @@
 package com.user.certifications.config;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import com.user.certifications.entities.Certification;
 import com.user.certifications.entities.User;
+import com.user.certifications.repositories.CertificationRepository;
 import com.user.certifications.repositories.UserRepository;
 import jakarta.annotation.PostConstruct;
+import org.hibernate.annotations.CurrentTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
 
 
 @Component
@@ -15,10 +27,14 @@ public class DataInitializer {
     private UserRepository userRepository;
 
     @Autowired
+    private CertificationRepository certificationRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @PostConstruct
-    public void init() {
+    public void init() throws FileNotFoundException {
+        createCertifications();
         if (userRepository.findByUsername("admin@gmail.com").isEmpty()) {
             User user = new User();
             user.setName("Admin");
@@ -29,6 +45,30 @@ public class DataInitializer {
             System.out.println("Initialized user Admin with email mansoor@gmail.com and encoded password.");
         } else {
             System.out.println("User with email admin@gmail.com already exists.");
+        }
+    }
+    private void createCertifications() throws FileNotFoundException {
+        String filePath="src/main/java/com/user/certifications/config/certifications.csv";
+        try(CSVReader reader = new CSVReader(new FileReader(filePath))){
+            List<String[]> records = reader.readAll();
+            for (int i = 1; i < records.size();i++){
+                String[] record = records.get(i);
+                if (!certificationRepository.existsByHyperlink(record[2])){
+                    Certification cert = new Certification();
+                    cert.setName(record[0]);
+                    cert.setAbout(record[1]);
+                    cert.setHyperlink(record[2]);
+                    cert.setSkills(record[3]);
+                    cert.setExamBoard(record[4]);
+                    cert.setLearningTime(record[5]);
+                    cert.setDateAdded(new Date());
+                    certificationRepository.save(cert);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CsvException e) {
+            throw new RuntimeException(e);
         }
     }
 }
